4f9d7f060e99e539db0a18b5674f1b95979d2a67
ssm123ssm <ssm123ssm@gmail.com>
2019-07-07 19:05:28 +0530

    server code made invisible

--- server.js
+++ server.js
@@ -106,7 +106,10 @@ var dbFunctions = {
 };
 
 
-app.use(express.static('./'));
+app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
 
 //PUSH
 function push(sub, obj) {
